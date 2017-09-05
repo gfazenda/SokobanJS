019eec6a6ec8a3165e01620883d217257af22a08
gfazenda <gabriel.l.fazenda@gmail.com>
2017-09-04 23:29:48 -0300

    targets working

--- Controller.js
+++ Controller.js
@@ -16,23 +16,34 @@ app.controller('myCtrl', function($scope) {
 		3,4,3,3,2,0,0,0,3,0,
 		3,0,3,0,4,0,0,0,3,0,
 		3,2,0,4,2,2,0,0,3,3,
-		3,0,0,0,4,0,0,0,4,3,
+		3,0,0,0,4,0,0,0,0,3,
 		3,0,0,0,0,0,0,0,0,3,
 		3,3,3,3,3,3,3,3,3,3
 		];
 		
 		$scope.points = 0;
 		$scope.playerPosition = {x: 0, y:0};
+		$scope.targets = [];
 		
 		$scope.StartMatrix = function(){
 			
-			// $scope.scene[$scope.GetIndex(4,4)] = 2;
+			$scope.targets.push(new $scope.Coord(1,2));
+			$scope.targets.push(new $scope.Coord(1,4));
+			$scope.targets.push(new $scope.Coord(4,5));
+			$scope.targets.push(new $scope.Coord(3,6));
+			$scope.targets.push(new $scope.Coord(4,7));
+			$scope.targets.push(new $scope.Coord(7,3));
 
 			$scope.playerPosition = {x: 2, y:2};
 			$scope.scene[$scope.GetIndex(2,2)] = 1;
 		}
 		$scope.GetIndex = function(x,y){
 			return (x + y * $scope.rows);
+		}
+
+		$scope.Coord = function(x,y){
+			this.x = x;
+			this.y = y;
 		}
 		
 		$scope.MovePlayer = function(dir){
@@ -58,19 +69,7 @@ app.controller('myCtrl', function($scope) {
 			
 			if(playerPos.x >= 0 && playerPos.x < $scope.columns 
 				&& playerPos.y >=0 && playerPos.y < $scope.rows){					
-					// if($scope.scene[playerPos.x][playerPos.y] == 'e'){					
-						// $scope.scene[$scope.playerPosition.x][$scope.playerPosition.y] = 'e';	
-						// $scope.playerPosition = playerPos;
-						// $scope.scene[$scope.playerPosition.x][$scope.playerPosition.y] = 'p';
-					// }
-					// else if($scope.scene[playerPos.x][playerPos.y] == 'b' && $scope.MoveBox(dir,playerPos)){					
-						// $scope.scene[$scope.playerPosition.x][$scope.playerPosition.y] = 'e';	
-						// $scope.playerPosition = playerPos;
-						// $scope.scene[$scope.playerPosition.x][$scope.playerPosition.y] = 'p';
-					// }
-					// console.log($scope.GetIndex(playerPos.x,playerPos.y));
-					// console.log($scope.GetIndex(5,5));
-					if($scope.scene[$scope.GetIndex(playerPos.x,playerPos.y)] == 0){					
+					if($scope.scene[$scope.GetIndex(playerPos.x,playerPos.y)] == 0 || $scope.scene[$scope.GetIndex(playerPos.x,playerPos.y)] == 4){					
 						$scope.scene[$scope.GetIndex($scope.playerPosition.x,$scope.playerPosition.y)] = 0;	
 						$scope.playerPosition = playerPos;
 						$scope.scene[$scope.GetIndex($scope.playerPosition.x,$scope.playerPosition.y)] = 1;
@@ -79,7 +78,8 @@ app.controller('myCtrl', function($scope) {
 						$scope.scene[$scope.GetIndex($scope.playerPosition.x,$scope.playerPosition.y)] = 0;	
 						$scope.playerPosition = playerPos;
 						$scope.scene[$scope.GetIndex($scope.playerPosition.x,$scope.playerPosition.y)] = 1;
-					}					
+					}	
+					$scope.CheckTargets();				
 			}
 			
 			console.log($scope.playerPosition);
@@ -103,19 +103,31 @@ app.controller('myCtrl', function($scope) {
 			}
 			if(position.x >= 0 && position.x < $scope.columns 
 				&& position.y >=0 && position.y < $scope.rows){							
-					if($scope.scene[$scope.GetIndex(position.x,position.y)] == 0){
-						console.log('moved box');
+					if($scope.scene[$scope.GetIndex(position.x,position.y)] == 0 || $scope.scene[$scope.GetIndex(position.x,position.y)] == 4){
 						$scope.scene[$scope.GetIndex(position.x,position.y)] = 2;
 						return true;
-					}	
-					else if($scope.scene[$scope.GetIndex(position.x,position.y)] == 4){
-						console.log('moved box');
-						$scope.scene[$scope.GetIndex(position.x,position.y)] = 2;
-						$scope.points ++;
-						return true;
-					}							
+					}								
 			}
 			return false;
+		}
+
+
+		$scope.CheckTargets = function(){
+			var tCount = $scope.targets.length;
+			$scope.points = 0; 
+			console.log('thing ' + tCount);
+			for(i = 0; i < $scope.targets.length; i++){
+				currentTarget = $scope.targets[i];
+				if($scope.scene[$scope.GetIndex(currentTarget.x,currentTarget.y)] == 0){
+					$scope.scene[$scope.GetIndex(currentTarget.x,currentTarget.y)] = 4;
+				}else if ($scope.scene[$scope.GetIndex(currentTarget.x,currentTarget.y)] == 2){
+					tCount -= 1;
+					$scope.points++;
+					if(tCount <= 0){
+						console.log('YOU WON!!!!');
+					}
+				}
+			}
 		}
 		
 		$scope.ResetMatrix = function() {
